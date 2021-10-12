6ca7a930f14cd4897632f90d64046e5b6465b981
Nikbakht, Saeed <nikbakhts@hotmail.com>
2021-10-12 10:14:37 -0300

    Passed the failing test and added a new one

--- Reversi.UnitTests/ConsolTest.cs
+++ Reversi.UnitTests/ConsolTest.cs
@@ -321,10 +321,14 @@ namespace Reversi.UnitTests
             var reversi = new Game(board, 'B');
             Square selectedSquare = new Square(0, 3);
 
-            Assert.IsTrue(reversi.PlaceCounter(selectedSquare));
+            Assert.IsFalse(reversi.PlaceCounter(selectedSquare));
+        }
 
-            var expected = new string[]{
-                    "...B....",
+        [Test]
+        public void IfEmptyBoardAcceptCentreSquareForPlacement1()
+        {
+            var board = new string[]{
+                    "........",
                     "........",
                     "........",
                     "........",
@@ -333,8 +337,22 @@ namespace Reversi.UnitTests
                     "........",
                     "........"};
 
+            var reversi = new Game(board, 'B');
+            Square selectedSquare = new Square(3, 4);
+
+            Assert.IsTrue(reversi.PlaceCounter(selectedSquare));
+
+            var expected = new string[]{
+                    "........",
+                    "........",
+                    "........",
+                    "....B...",
+                    "........",
+                    "........",
+                    "........",
+                    "........"};
+
             CollectionAssert.AreEqual(expected, reversi.ReversiBoard.GetCurrentState());
         }
-
     }
 }
