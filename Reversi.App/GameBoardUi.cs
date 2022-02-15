2a32b974022b5fdde06f628d17d85f3b5eecd848
Stephen Somerville <stephen.s@geoffreystephens.ca>
2022-02-15 09:47:28 -0400

    Renamed GameboardUi Moved all functions to gameboardUI from program

---
+++ Reversi.App/GameBoardUi.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Reversi.Logic;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Reversi.App
+{
+    public class GameBoardUi
+    {
+        static IEnumerable<string> guidedBoard;
+
+        public void SetupBoard(Game reversi)
+        {
+            var board = reversi.DisplayBoard();
+            guidedBoard = PrependGuidesToStringArrays(board);
+        }
+ 
+        public IEnumerable<string> PrependGuidesToStringArrays(string[] Original)
+        {
+            string[] guidedArray = new string[8];
+            int counter = 0;
+
+            foreach (string S in Original)
+            {
+                guidedArray[counter] = counter + S;
+                counter++;
+            }
+            var finalArray = guidedArray.Prepend(" 01234567");
+
+            return finalArray;
+        }
+        public void DisplayBoard(Game reversi)
+        {
+            var currentPlayer = (reversi.GetCurrentPlayer() == Constants.BLACK) ? "BLACK" : "WHITE";
+
+            Console.WriteLine(string.Join('\n', guidedBoard));
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            if (reversi.State.MoveInvalid)
+                WriteErrorMessage("Invalid Move!");
+            if (reversi.State.PassOccured)
+                WriteErrorMessage("User had no possible moves. Turn passed!");
+            if (reversi.State.GameOver)
+                WriteErrorMessage("Game Over!");
+
+            Console.WriteLine("Current turn: " + currentPlayer);
+            Console.Write("Enter coordinates (row,col): ");
+        }
+        public void GetPlayerInput(Game reversi)
+        {
+            var coords = Console.ReadLine();
+            var coordsSplit = coords.Split(',');
+            reversi.PlaceCounter(int.Parse(coordsSplit[0]), int.Parse(coordsSplit[1]));
+            Console.Clear();
+        }
+        private void WriteErrorMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
--- Reversi.App/Program.cs
+++ Reversi.App/Program.cs
@@ -5,59 +5,22 @@ using Reversi.Logic;
 
 namespace Reversi.App
 {
-    public class Program : GameBoard
+    public class Program
     {
-        static IEnumerable<string> guidedBoard;
-        static string currentPlayer;
 
         static void Main(string[] args)
         {
             var reversi = new Game();
-            var gameBoard = new GameBoard();
+            var gameBoardUi = new GameBoardUi();
 
             do
             {
-                gameBoard.SetupBoard(reversi);
-                DisplayBoard(reversi);
-
-                GetPlayerInput(reversi);
+                gameBoardUi.SetupBoard(reversi);
+                gameBoardUi.DisplayBoard(reversi);
+                gameBoardUi.GetPlayerInput(reversi);
             } while (!reversi.State.GameOver);
         }
 
-        private static void GetPlayerInput(Game reversi)
-        {
-            var coords = Console.ReadLine();
-            var coordsSplit = coords.Split(',');
-            reversi.PlaceCounter(int.Parse(coordsSplit[0]), int.Parse(coordsSplit[1]));
-            Console.Clear();
-        }
-
-        private static void DisplayBoard(Game reversi)
-        {
-            currentPlayer = (reversi.GetCurrentPlayer() == Constants.BLACK) ? "BLACK" : "WHITE";
-
-            Console.WriteLine(string.Join('\n', guidedBoard));
-            Console.WriteLine();
-            Console.WriteLine();
-
-            
-            if (reversi.State.MoveInvalid)
-                WriteErrorMessage("Invalid Move!");
-            if (reversi.State.PassOccured)
-                WriteErrorMessage("User had no possible moves. Turn passed!");
-            if (reversi.State.GameOver)
-                WriteErrorMessage("Game Over!");
-
-            Console.WriteLine("Current turn: " + currentPlayer);
-            Console.Write("Enter coordinates (row,col): ");
-        }
-
-        private static void WriteErrorMessage(string message)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(message);
-            Console.ForegroundColor = ConsoleColor.White;
-        }
 
     }
 }
