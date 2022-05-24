a36f29c850982e640aa2be973e702e4772209c10
TeamOsprey <89081537+TeamOsprey@users.noreply.github.com>
2022-05-24 09:48:43 -0300

    WIP: assigning players to connections

--- Reversi.Web/Hubs/GameHub.cs
+++ Reversi.Web/Hubs/GameHub.cs
@@ -10,5 +10,12 @@ namespace Reversi.Web.Hubs
             await Clients.All.SendAsync("ReceiveUpdate");
             // await Clients.Client(player).SendAsync("ReceiveUpdate"); work was started to recognize the player
         }
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "Game1");
+            // assign color to user
+            // if BLACK's connection ID is null then set to Context.ConnectionId
+            await base.OnConnectedAsync();
+        }
     }
 }
