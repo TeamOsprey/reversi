3afb44ab5a93caaaba8ade2681cefcf1c9dac873
jcs-instructor <jcsilberman@gmail.com>
2022-11-01 09:07:40 -0400

    WIP: Added comments - trying to set the ClaimsPrincipal if not set yet

--- Reversi.Web/Hubs/GameHub.cs
+++ Reversi.Web/Hubs/GameHub.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Net.Sockets;
+using System.Security.Principal;
 using System.Threading.Tasks;
 
 namespace Reversi.Web.Hubs
@@ -8,7 +12,27 @@ namespace Reversi.Web.Hubs
     public class GameHub : Hub
     {
         //        public string UserId => Context.UserIdentifier;
-        public string UserId => "player" + DateTime.Now.ToString();
+
+        public string UserId => "player1";
+        //{
+        //    get
+        //    {
+        //        if (Context.User == null)
+        //        {
+        //            HttpContext.User = new GenericPrincipal(new FormsIdentity(ticket), roles);
+        //            HttpContext.Current.User = new System.Security.Principal.GenericPrincipal(
+        //                new System.Security.Principal.GenericIdentity(person.LoginName),
+        //                new string[] { /* fill roles if any */ });
+        //        }
+        //        //HttpContext.Current.User = new System.Security.Principal.GenericPrincipal(
+        //        //    new System.Security.Principal.GenericIdentity(person.LoginName),
+        //        //    new string[] { /* fill roles if any */ });
+        //    }
+        //    set
+        //    {
+        //        throw new NotImplementedException();
+        //    }
+        //}
 
         public async Task SendUpdate()
         {
