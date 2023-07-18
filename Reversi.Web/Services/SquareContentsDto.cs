42f18606f608a035ed7a4d0409a5f61716de1744
Nikbakht, Saeed <Saeed.Nikbakht@novascotia.ca>
2023-07-18 09:41:39 -0300

    Removed UI dependency on SquareContents

---
+++ Reversi.Web/Services/SquareContentsDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Reversi.Web.Services
+{
+    public static class SquareContentsDto
+    {
+        public const char Black = 'B';
+        public const char White = 'W';
+        public const char None = '.';
+        public const char Legal = '0';
+    }
+}
