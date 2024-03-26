254f9ca7f088d99f3f5c779ca75a2dece1751b88
jcs-instructor <jcsilberman@gmail.com>
2024-03-26 09:27:05 -0400

    WIP: Moved Room.cshtml to Pages folder and created links in Lobby (neither working)

--- Reversi.Web/Pages/Room.cshtml.cs
+++ Reversi.Web/Pages/Room.cshtml.cs
@@ -1,12 +1,11 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Reversi.WebApp.Views.Room
+namespace Reversi.WebApp.Pages
 {
     public class RoomModel : PageModel
     {
         public string UserId { get; set; }
-        
+
         public void OnGet(string userId)
         {
             UserId = userId;
